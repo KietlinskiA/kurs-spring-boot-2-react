881c55a396eba3c18394c00d829453fd4a49bf03
KietlinskiA <adrian.kietlinski@interia.pl>
2023-08-15 13:35:50 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './img/1.png';
 import './App.css';
+import Car from './Car';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Się obraca sztanga</p>
+        Naucz się Reacta
+        <Car></Car>
       </header>
     </div>
   );
